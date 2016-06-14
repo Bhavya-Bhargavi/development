6cf906115e66d70d967b116fb3b683847b103cfc
Bhavya <Bhavya>
2016-06-14 09:23:39 +0530

    added some files into git

---
+++ click/assignment.js
@@ -0,0 +1,21 @@
+
+	function formcalling(){
+
+		var firstName= document.getElementById("firstname");
+		var lastName= document.getElementById("lastname");
+		var age= document.getElementById("number");
+		var gender= document.getElementById("gender");
+		
+			firstName.value= "wrong name";
+			lastName.value = "wrong last name";
+			age.value = "50";
+			gender.value = "male";
+
+			switch(form-field){
+				case 'firstName':
+				case 'lastName':
+				case 'age':
+					
+
+			}
+	};
